8328e1028408d44b07dd50ca0c07d743a5589a23
Daniel Luiz <danieluiz1607@gmail.com>
2026-02-11 14:12:54 -0300

    create file SistemaPagamento

---
+++ src/main/java/br/com/daniel/modulo2/SistemaPagamento.java
@@ -0,0 +1,28 @@
+package br.com.daniel.modulo2;
+
+// O CONTRATO
+interface ProcessadorPagamento {
+    void processar(double valor); // Todo processador TEM que ter esse método
+}
+
+// IMPLEMENTAÇÃO 1
+class Pix implements ProcessadorPagamento {
+    public void processar(double valor) {
+        System.out.println("Gerando QRCode para R$" + valor);
+    }
+}
+
+// IMPLEMENTAÇÃO 2
+class Cartao implements ProcessadorPagamento {
+    public void processar(double valor) {
+        System.out.println("Chamando operadora de cartão para R$" + valor);
+    }
+}
+
+class Loja {
+    // A Loja não depende de Pix ou Cartão. Depende da INTERFACE (Abstração).
+    // Isso é Injeção de Dependência (Princípio do SOLID).
+    public void finalizarCompra(ProcessadorPagamento meioPagamento, double valor) {
+        meioPagamento.processar(valor);
+    }
+}
