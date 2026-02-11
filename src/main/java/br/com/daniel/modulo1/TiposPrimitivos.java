2d3d1ce18bcd1c19b5e297f6ffdb551f870e89be
Daniel Luiz <danieluiz1607@gmail.com>
2026-02-11 13:29:38 -0300

    add code of TiposPrimitivos

--- src/main/java/br/com/daniel/modulo1/TiposPrimitivos.java
+++ src/main/java/br/com/daniel/modulo1/TiposPrimitivos.java
@@ -1,5 +1,23 @@
-package br.com.daniel.modulo1
+package br.com.daniel.modulo1;
 
-public class TiposPrimitivos {
-    
+public class TiposMemoria {
+    public static void main(String[] args) {
+        // --- PRIMITIVOS (Performance e Segurança) ---
+        int idadePrimitiva = 25;
+        // int idadeNula = null; // ERRO: Primitivo não aceita null.
+        
+        // --- WRAPPERS (Flexibilidade e Null) ---
+        Integer idadeWrapper = 25; // Autoboxing (Java converte int para Integer sozinho)
+        Integer idadeNula = null;  // Permitido. Usado em Bancos de Dados quando o campo é opcional.
+
+        // --- O PERIGO DA COMPARAÇÃO ---
+        int a = 1000;
+        int b = 1000;
+        System.out.println(a == b); // TRUE (Compara valor: 1000 == 1000)
+
+        Integer c = 1000;
+        Integer d = 1000;
+        System.out.println(c == d); // FALSE! (Compara endereço de memória de dois objetos diferentes)
+        System.out.println(c.equals(d)); // TRUE (Jeito certo: Compara o CONTEÚDO do objeto)
+    }
 }
