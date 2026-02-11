281a52f04c361994328d7967e032d325723a59e9
Daniel Luiz <danieluiz1607@gmail.com>
2026-02-11 13:55:49 -0300

    create file TesteCarro

---
+++ src/main/java/br/com/daniel/modulo2/TesteCarro.java
@@ -0,0 +1,22 @@
+package br.com.daniel.modulo2;
+import br.com.daniel.modulo2.Carro
+
+// 2. A fábrica (Onde a mágica acontece)
+public class TesteCarro {
+    public static void main(String[] args) {
+        // 'c1' é uma REFERÊNCIA na Stack.
+        // 'new Carro()' aloca memória na HEAP.
+        Carro c1 = new Carro(); 
+        c1.modelo = "Fusca";
+        
+        Carro c2 = new Carro();
+        c2.modelo = "Civic";
+        
+        // CUIDADO: Atribuição de referência, não cópia de objeto!
+        Carro c3 = c1; 
+        c3.modelo = "Brasilia"; 
+        
+        // O que imprime aqui?
+        System.out.println(c1.modelo); // "Brasilia" (Porque c1 e c3 apontam para o MESMO objeto)
+    }
+}
