ded9b715695b925bd6a001a4a572d73ad9735b10
Daniel Luiz <danieluiz1607@gmail.com>
2026-02-11 14:31:34 -0300

    add abstract class Produto

---
+++ src/main/java/br/com/daniel/mini-ecommerce/Produto.java
@@ -0,0 +1,14 @@
+package br.com.daniel.mini-ecommerce;
+
+public abstract class Produto {
+    protected String nome;
+    protected double preco;
+
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() { return nome; }
+    public double getPreco() { return preco; }
+}
