b452c63bcf45bbcc580bf374f4e2fda7eb47482f
Daniel Luiz <danieluiz1607@gmail.com>
2026-02-12 11:26:51 -0300

    create exercises of interfeces - 01

---
+++ src/main/java/br/com/daniel/exercises/interfaces/ex01/Retangulo.java
@@ -0,0 +1,14 @@
+public class Retangulo {
+    private double altura;
+    private double largura;
+
+    public Retangulo(double altura, double largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    @Override
+    public double getArea(){
+        return altura * largura;
+    }
+}
