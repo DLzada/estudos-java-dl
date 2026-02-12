a1bf423f2410f01b1014d30cace8352c6259c8fc
Daniel Luiz <danieluiz1607@gmail.com>
2026-02-12 11:37:56 -0300

    add Main of exercise one

---
+++ src/main/java/br/com/daniel/exercises/interfaces/ex01/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Instaciando Classes
+        Circulo circulo = new Circulo(3);
+        Triangulo triangulo = new Triangulo(3, 6);
+        Retangulo retangulo = new Retangulo(3, 5);
+
+        // Exibindo instancias
+        System.out.println("A área do círculo é: " +  circulo.getArea());
+        System.out.println("A área do triangulo é: " + triangulo.getArea());
+        System.out.println("A área do retangulo é: " + retangulo.getArea());
+    }
+}
