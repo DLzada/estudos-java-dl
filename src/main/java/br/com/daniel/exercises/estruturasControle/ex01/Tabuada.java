bff06e40f0a5358ed66b1038df302d4760daa86a
Daniel Luiz <danieluiz1607@gmail.com>
2026-03-16 11:04:34 -0300

    feat: ex01 of estruturasControles

---
+++ src/main/java/br/com/daniel/exercises/estruturasControle/ex01/Tabuada.java
@@ -0,0 +1,21 @@
+package estruturasControle.ex01;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        //Escreva um codigo onde o usuario entra com um número e seja gerada a tabuada de 1 até 10 desse número
+
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero para saber sua tabuada: ");
+        int numero = scanner.nextInt();
+        var resultado = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            resultado = numero * i;
+            System.out.printf("%s x %s = %s \n", numero, i, resultado);
+        }
+
+    }
+}
