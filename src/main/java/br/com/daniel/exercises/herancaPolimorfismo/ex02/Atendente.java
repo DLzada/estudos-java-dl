ff544a92a75b171ce019d237286d98fe671980fb
Daniel Luiz <danieluiz1607@gmail.com>
2026-03-18 10:42:14 -0300

    feat: add class Atendente in ex02

---
+++ src/main/java/br/com/daniel/exercises/herancaPolimorfismo/ex02/Atendente.java
@@ -0,0 +1,24 @@
+package herancaPolimorfismo.ex02;
+
+public class Atendente extends Usuario{
+    private double valorEmCaixa;
+
+    public Atendente(String nome, boolean administrador, String senha, String email, double valorEmCaixa) {
+        super(nome, administrador, senha, email);
+        this.valorEmCaixa = 0.0;
+    }
+
+    public void receberPagamento(double valor){
+        this.valorEmCaixa += valor;
+        System.out.println("Pagamento de R$" + valor + " recebido");
+    }
+
+    public void fecharCaixa(){
+        System.out.println("Caixa Fechado com o valor total de: R$" + valorEmCaixa);
+        this.valorEmCaixa = 0.0;
+    }
+
+    public double getValorEmCaixa(){
+        return valorEmCaixa;
+    }
+}
