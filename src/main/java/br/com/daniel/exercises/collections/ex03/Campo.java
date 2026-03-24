8b624f7adf741794c8436984ebaa5be7e04d53bd
Daniel Luiz <danieluiz1607@gmail.com>
2026-03-24 11:40:20 -0300

    feat: create file Campo in ex03

---
+++ src/main/java/br/com/daniel/exercises/collections/ex03/Campo.java
@@ -0,0 +1,17 @@
+package collections.ex03;
+
+public class Campo {
+    private String nome;
+    private String valor;
+    private String tipo;
+
+    public Campo(String nome, String valor, String tipo) {
+        this.nome = nome;
+        this.valor = valor;
+        this.tipo = tipo.toLowerCase();
+    }
+
+    public String getNome() { return nome; }
+    public String getValor() { return valor; }
+    public String getTipo() { return tipo; }
+}
