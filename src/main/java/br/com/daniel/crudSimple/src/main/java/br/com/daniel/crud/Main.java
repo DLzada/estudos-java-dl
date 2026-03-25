0544aad73c42712d5846d34114ff8660502de756
Daniel Luiz <danieluiz1607@gmail.com>
2026-03-25 09:48:01 -0300

    feat: create MenuOption e modification Main

--- src/main/java/br/com/daniel/crudSimple/src/main/java/br/com/daniel/crud/Main.java
+++ src/main/java/br/com/daniel/crudSimple/src/main/java/br/com/daniel/crud/Main.java
@@ -1,17 +1,27 @@
 package br.com.daniel.crud;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import br.com.daniel.crud.dao.UserDAO;
+import br.com.daniel.crud.model.MenuOption;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+public class Main {
+
+    private final static UserDAO dao = new UserDAO();
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Bem vindo ao cadastro de usuários, selecione a operação desejada");
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Atualizar");
+        System.out.println("3 - Excluir");
+        System.out.println("4 - Buscar por identificador");
+        System.out.println("5 - Listar");
+        System.out.println("6 - Sair");
+
+        var userInput = scanner.nextInt();
+//        while (true){
+//            var selectOption = MenuOption.values()[userInput -1];
+//        }
     }
 }
