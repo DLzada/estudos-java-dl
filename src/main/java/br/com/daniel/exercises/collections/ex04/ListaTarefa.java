f80b516013351dd23ea21b74d08f049d3ecac927
Daniel Luiz <danieluiz1607@gmail.com>
2026-03-29 08:32:41 -0300

    final: completed ex04 of collections

--- src/main/java/br/com/daniel/exercises/collections/ex04/ListaTarefa.java
+++ src/main/java/br/com/daniel/exercises/collections/ex04/ListaTarefa.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaTarefa {
+    /*
+    Crie uma classe chamada "ListaTarefas" que possui uma lista de tarefas como atributo. Cada tarefa é representada por uma classe chamada "Tarefa" que possui um atributo de descrição. Implemente os seguintes métodos:
+
+        adicionarTarefa(String descricao): Adiciona uma nova tarefa à lista com a descrição fornecida.
+        removerTarefa(String descricao): Remove uma tarefa da lista com base em sua descrição.
+        obterNumeroTotalTarefas(): Retorna o número total de tarefas na lista.
+        obterDescricoesTarefas(): Retorna uma lista contendo a descrição de todas as tarefas na lista.
+
+    */
     private List<Tarefa> tarefaList;
 
     public ListaTarefa() {
@@ -30,5 +39,20 @@ public class ListaTarefa {
 
     public void obterDescricaoTarefas(){
         System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("tarefa 1");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricaoTarefas();
     }
 }
