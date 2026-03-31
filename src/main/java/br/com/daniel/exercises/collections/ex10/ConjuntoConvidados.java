7e508db47709b64ab63ff3dcfa2985557fc727f6
Daniel Luiz <danieluiz1607@gmail.com>
2026-03-31 09:01:11 -0300

    feat: add method main in ConjuntoConvidados ex10

--- src/main/java/br/com/daniel/exercises/collections/ex10/ConjuntoConvidados.java
+++ src/main/java/br/com/daniel/exercises/collections/ex10/ConjuntoConvidados.java
@@ -34,10 +34,10 @@ public class ConjuntoConvidados {
                 break;
             }
         }
-        convidadoSet.remove(convidadoParaRemover)
+        convidadoSet.remove(convidadoParaRemover);
     }
 
-    public int contarConvidado(){
+    public int contarConvidados(){
         return convidadoSet.size();
     }
 
@@ -45,4 +45,26 @@ public class ConjuntoConvidados {
         System.out.println(convidadoSet);
     }
 
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        // Adicionando convidados ao conjunto
+        conjuntoConvidados.adicionarConvidado("Alice", 1234);
+        conjuntoConvidados.adicionarConvidado("Bob", 1235);
+        conjuntoConvidados.adicionarConvidado("Charlie", 1235);
+        conjuntoConvidados.adicionarConvidado("David", 1236);
+
+        System.out.println("Convidados no conjunto:");
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1236);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados após a remoção");
+
+        System.out.println("Convidados no conjunto após a remoção:");
+        conjuntoConvidados.exibirConvidados();
+    }
 }
