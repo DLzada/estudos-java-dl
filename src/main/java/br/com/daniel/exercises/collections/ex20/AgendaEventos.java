9eb6c0275dfedef83ebbbe3e3eb2904328ccf628
Daniel Luiz <danieluiz1607@gmail.com>
2026-04-01 11:00:24 -0300

    feat: resolution ex20 collections

---
+++ src/main/java/br/com/daniel/exercises/collections/ex20/AgendaEventos.java
@@ -0,0 +1,68 @@
+package collections.ex20;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AgendaEventos {
+    /*
+    Agenda de Eventos
+        Crie uma classe chamada "AgendaEventos" que utilize um `Map` para armazenar as datas e seus respectivos Eventos. Cada evento é representado por um objeto da classe "Evento", que possui atributos como nome do evento e o nome da atração. Implemente os seguintes métodos:
+
+        adicionarEvento(LocalDate data, String nome, String atracao): Adiciona um evento à agenda.
+        exibirAgenda(): Exibe a agenda de eventos em ordem crescente de data.
+        obterProximoEvento(): Retorna o próximo evento que ocorrerá.
+    */
+
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        //Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+            if (dataEvento.isEqual(dataAtual) || dataEvento.isAfter(dataAtual)) {
+                proximaData = dataEvento;
+                proximoEvento = entry.getValue();
+                break;
+            }
+        }
+        if (proximoEvento != null) {
+            System.out.println("O próximo evento: " + proximoEvento.getNome() + " acontecerá na data " + proximaData);
+        } else {
+            System.out.println("Não há eventos futuros na agenda.");
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2025, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2025, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2025, Month.AUGUST, 28), "Hackathon de Inovação", "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2028, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+
+}
