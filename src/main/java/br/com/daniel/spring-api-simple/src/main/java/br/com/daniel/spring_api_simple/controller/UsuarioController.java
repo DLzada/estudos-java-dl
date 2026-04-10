5c00de881242b8d9333f7b6eebf0606459460600
Daniel Luiz <danieluiz1607@gmail.com>
2026-04-10 12:00:18 -0300

    feat: add documention with swagger and add method put

--- src/main/java/br/com/daniel/spring-api-simple/src/main/java/br/com/daniel/spring_api_simple/controller/UsuarioController.java
+++ src/main/java/br/com/daniel/spring-api-simple/src/main/java/br/com/daniel/spring_api_simple/controller/UsuarioController.java
@@ -26,4 +26,9 @@ public class UsuarioController {
     public void postUser(@RequestBody Usuario usuario){
         repository.save(usuario);
     }
+
+    @PutMapping
+    public void putUser(@RequestBody Usuario usuario){
+        repository.update(usuario);
+    }
 }
