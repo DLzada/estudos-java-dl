e50973f105662bc7ffc2573b2a61c5608b616a8a
Daniel Luiz <danieluiz1607@gmail.com>
2026-04-10 10:49:52 -0300

    feat: commit initial of spring-api-simple

---
+++ src/main/java/br/com/daniel/spring-api-simple/src/main/java/br/com/daniel/spring_api_simple/SpringApiSimpleApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.spring_api_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiSimpleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/daniel/spring-api-simple/src/test/java/br/com/daniel/spring_api_simple/SpringApiSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.spring_api_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
