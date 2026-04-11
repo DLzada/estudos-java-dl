71dacf9d2575d0caa812588ef49639b95fdf54b7
Daniel Luiz <danieluiz1607@gmail.com>
2026-04-11 10:16:56 -0300

    feat: add controller and modification in webConfigSecurity

---
+++ src/main/java/br/com/daniel/spring-security-simple/src/main/java/br/com/daniel/spring_security_simple/WebConfigSecurity.java
@@ -0,0 +1,56 @@
+package br.com.daniel.spring_security_simple;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebConfigSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/managers").hasRole("MANAGERS")
+                        .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user01")
+                .password("{noop}123")
+                .roles("USERS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("daniel")
+                .password("{noop}senha123")
+                .roles("MANAGERS")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
