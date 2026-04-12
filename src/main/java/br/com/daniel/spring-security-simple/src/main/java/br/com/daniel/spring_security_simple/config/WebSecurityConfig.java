fbc2c37cf23a34fc0531243f9daf33f4eaf4155b
dlzada <danielluiz1607@gmail.com>
2026-04-12 08:50:09 -0300

    feat: add UserRepository and WebSecurityConfig

---
+++ src/main/java/br/com/daniel/spring-security-simple/src/main/java/br/com/daniel/spring_security_simple/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package br.com.daniel.spring_security_simple.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/managers").hasRole("MANAGERS")
+                        .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/br/com/daniel/spring-security-simple/src/main/java/br/com/daniel/spring_security_simple/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.daniel.spring_security_simple.repository;
+
+
+import br.com.daniel.spring_security_simple.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+}
