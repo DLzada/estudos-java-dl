34b624df42979e2d05f6923c34731283a81ba17b
dlzada <danielluiz1607@gmail.com>
2026-04-23 07:56:49 -0300

    feat: add project spring-produtos

---
+++ src/main/java/br/com/daniel/spring-produtos/src/main/java/br/com/daniel/spring_produtos/SpringProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.spring_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProdutosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/daniel/spring-produtos/src/test/java/br/com/daniel/spring_produtos/SpringProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.spring_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
