ef7834808b07f6baa716e19f1db3fe8dd2506280
dlzada <danielluiz1607@gmail.com>
2026-04-24 08:40:44 -0300

    completion of mini project spring-produtos

--- src/main/java/br/com/daniel/spring-produtos/src/main/java/br/com/daniel/spring_produtos/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/daniel/spring-produtos/src/main/java/br/com/daniel/spring_produtos/handler/GlobalExceptionHandler.java
@@ -18,4 +18,14 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handlerExceptions(Exception ex){
+        ErrorResponse response = ErrorResponse.builder()
+                .message(ex.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
 }
