c6ef2f7a6208fa81c36279c4fcf877a422a2138c
dlzada <danielluiz1607@gmail.com>
2026-04-26 09:04:02 -0300

    feat: modification of query method in IAlunosRepository

--- src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/database/repository/IAlunosRespository.java
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/database/repository/IAlunosRespository.java
@@ -3,5 +3,8 @@ package br.com.daniel.spring_produtos.database.repository;
 import br.com.daniel.spring_produtos.database.model.AlunosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IAlunosRespository extends JpaRepository<AlunosEntity, Integer> {
+    Optional<AlunosEntity> findByEmail(String email);
 }
