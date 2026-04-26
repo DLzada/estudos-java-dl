1fbccd8c8bc8930f1350f74fb6feace52576ce50
dlzada <danielluiz1607@gmail.com>
2026-04-26 09:14:03 -0300

    feat: create AlunosController with route of criar aluno

---
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/controller/AlunosController.java
@@ -0,0 +1,30 @@
+package br.com.daniel.spring_produtos.controller;
+
+import br.com.daniel.spring_produtos.database.model.ExerciciosEntity;
+import br.com.daniel.spring_produtos.dto.AlunoDto;
+import br.com.daniel.spring_produtos.dto.ExercicioDto;
+import br.com.daniel.spring_produtos.exception.BadRequestException;
+import br.com.daniel.spring_produtos.service.AlunosService;
+import br.com.daniel.spring_produtos.service.ExerciciosService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/alunos")
+@RequiredArgsConstructor
+@Validated
+public class AlunosController {
+
+    private final AlunosService alunosService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAluno(@Valid @RequestBody AlunoDto alunoDto) throws BadRequestException  {
+        alunosService.criarAluno(alunoDto);
+    }
+}
