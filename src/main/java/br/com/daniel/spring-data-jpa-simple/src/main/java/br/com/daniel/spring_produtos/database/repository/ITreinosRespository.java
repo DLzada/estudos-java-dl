502fe5386b20e9b3df63cd40b201958b3faecf39
dlzada <danielluiz1607@gmail.com>
2026-04-27 08:30:40 -0300

    feat: create method criarTreino in TreinoService

--- src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/database/repository/ITreinosRespository.java
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/database/repository/ITreinosRespository.java
@@ -1,8 +1,10 @@
 package br.com.daniel.spring_produtos.database.repository;
 
-import br.com.daniel.spring_produtos.database.model.ExerciciosEntity;
 import br.com.daniel.spring_produtos.database.model.TreinosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ITreinosRespository extends JpaRepository<TreinosEntity, Integer> {
+    Optional<TreinosEntity>findByNomeAndAlunoId(String nome, Integer alunoId);
 }
--- src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/service/TreinoService.java
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/service/TreinoService.java
@@ -1,10 +1,19 @@
 package br.com.daniel.spring_produtos.service;
 
+import br.com.daniel.spring_produtos.database.model.AlunosEntity;
+import br.com.daniel.spring_produtos.database.model.ExerciciosEntity;
+import br.com.daniel.spring_produtos.database.model.TreinosEntity;
 import br.com.daniel.spring_produtos.database.repository.IAlunosRespository;
 import br.com.daniel.spring_produtos.database.repository.IExerciciosRespository;
 import br.com.daniel.spring_produtos.database.repository.ITreinosRespository;
+import br.com.daniel.spring_produtos.dto.TreinoDto;
+import br.com.daniel.spring_produtos.exception.BadRequestException;
+import br.com.daniel.spring_produtos.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +23,32 @@ public class TreinoService {
     private final IExerciciosRespository exerciciosRespository;
     private final ITreinosRespository treinosRespository;
 
+    public void criarTreino(TreinoDto treinoDto) throws NotFoundException, BadRequestException{
+        Set<ExerciciosEntity> exercicios = new HashSet<>();
+        AlunosEntity aluno = alunosRespository.findById(treinoDto.getAlunoId())
+                .orElseThrow(() -> new NotFoundException("Aluno nao encontrado"));
+
+        TreinosEntity treino = treinosRespository.findByNomeAndAlunoId(treinoDto.getNome(), treinoDto.getAlunoId())
+                .orElse(null);
+
+        if(treino != null){
+            throw new BadRequestException("Ja existe um treino com esse nome para esse aluno");
+        }
+
+        for(Integer exercicioId : treinoDto.getExerciciosIds()){
+            ExerciciosEntity exercicio = exerciciosRespository.findById(exercicioId)
+                    .orElseThrow(() -> new NotFoundException("Exercicio nao encontrado"));
+
+            exercicios.add(exercicio);
+        }
+
+        treino = TreinosEntity.builder()
+                .nome(treinoDto.getNome())
+                .aluno(aluno)
+                .exercicios(exercicios)
+                .build();
+
+        treinosRespository.save(treino);
+
+    }
 }
