a60bff40f851b278f2516259e51d7170ee30c2d2
dlzada <danielluiz1607@gmail.com>
2026-04-27 07:55:23 -0300

    feat: create route of get in AlunosController for students assessments

--- src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/controller/AlunosController.java
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/controller/AlunosController.java
@@ -1,9 +1,11 @@
 package br.com.daniel.spring_produtos.controller;
 
+import br.com.daniel.spring_produtos.database.model.AvaliacoesFisicasEntity;
 import br.com.daniel.spring_produtos.database.model.ExerciciosEntity;
 import br.com.daniel.spring_produtos.dto.AlunoDto;
 import br.com.daniel.spring_produtos.dto.ExercicioDto;
 import br.com.daniel.spring_produtos.exception.BadRequestException;
+import br.com.daniel.spring_produtos.exception.NotFoundException;
 import br.com.daniel.spring_produtos.service.AlunosService;
 import br.com.daniel.spring_produtos.service.ExerciciosService;
 import jakarta.validation.Valid;
@@ -26,5 +28,10 @@ public class AlunosController {
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAluno(@Valid @RequestBody AlunoDto alunoDto) throws BadRequestException  {
         alunosService.criarAluno(alunoDto);
+    }
+
+    @GetMapping("/{alunoId}/avaliacao")
+    public AvaliacoesFisicasEntity getAvaliacaoFisica(@PathVariable Integer alunoId) throws NotFoundException {
+        return alunosService.getAlunoAvaliacoes(alunoId);
     }
 }
