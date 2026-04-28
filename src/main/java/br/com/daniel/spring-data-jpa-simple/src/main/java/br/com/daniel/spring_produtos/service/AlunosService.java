e4247246daebf4e1e09b56bc9dbdeb38a611c825
dlzada <danielluiz1607@gmail.com>
2026-04-28 08:45:41 -0300

    feat: create method deletarAluno in service

--- src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/service/AlunosService.java
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/service/AlunosService.java
@@ -2,16 +2,24 @@ package br.com.daniel.spring_produtos.service;
 
 import br.com.daniel.spring_produtos.database.model.AlunosEntity;
 import br.com.daniel.spring_produtos.database.model.AvaliacoesFisicasEntity;
+import br.com.daniel.spring_produtos.database.model.TreinosEntity;
 import br.com.daniel.spring_produtos.database.repository.IAlunosRespository;
+import br.com.daniel.spring_produtos.database.repository.IAvaliacoesFisicasRespository;
+import br.com.daniel.spring_produtos.database.repository.ITreinosRespository;
 import br.com.daniel.spring_produtos.dto.AlunoDto;
 import br.com.daniel.spring_produtos.exception.BadRequestException;
 import br.com.daniel.spring_produtos.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AlunosService {
+    private final IAvaliacoesFisicasRespository avaliacoesFisicasRespository;
+    private final ITreinosRespository treinosRespository;
     private final IAlunosRespository alunosRespository;
 
     public void criarAluno(AlunoDto alunoDto) throws BadRequestException{
@@ -39,4 +47,24 @@ public class AlunosService {
         return avaliacao;
     }
 
+    @Transactional
+    public void deletarAuno(Integer alunoId) throws NotFoundException{
+        AlunosEntity aluno = alunosRespository.findById(alunoId)
+                .orElseThrow(() -> new NotFoundException("Aluno nao encontrada"));
+
+        //1.Deletar todos os treinos do aluno
+        List<Integer> treinosAlunoId  = aluno.getTreinos().stream()
+                .map(TreinosEntity::getId)
+                .toList();
+
+        treinosRespository.deleteAllById(treinosAlunoId);
+
+        //2. deletar o aluno
+        alunosRespository.deleteById(alunoId);
+
+
+        //3. deletar avaliacao fisica
+
+        alunosRespository.deleteById(aluno.getAvaliacaoFisica().getId());
+    }
 }
