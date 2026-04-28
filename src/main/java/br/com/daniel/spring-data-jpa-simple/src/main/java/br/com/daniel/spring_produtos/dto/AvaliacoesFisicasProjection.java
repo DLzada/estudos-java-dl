1165584e740f1a65a53099c9cff44ed2bc261eb8
dlzada <danielluiz1607@gmail.com>
2026-04-28 08:32:43 -0300

    feat: add Query for getAllAvaliacoes and getAllPageble and implemetetion in service and controller

--- src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/dto/AvaliacoesFisicasProjection.java
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/dto/AvaliacoesFisicasProjection.java
@@ -1,4 +1,12 @@
 package br.com.daniel.spring_produtos.dto;
 
+import java.math.BigDecimal;
+
 public interface AvaliacoesFisicasProjection {
+    Integer getIdAluno();
+    String getNomeAluno();
+    Integer getIdAvaliacao();
+    BigDecimal getPeso();
+    BigDecimal getAltura();
+    BigDecimal getPercentualGorduraCorporal();
 }
--- src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/service/AvaliacaoFisicaService.java
+++ src/main/java/br/com/daniel/spring-data-jpa-simple/src/main/java/br/com/daniel/spring_produtos/service/AvaliacaoFisicaService.java
@@ -5,16 +5,21 @@ import br.com.daniel.spring_produtos.database.model.AvaliacoesFisicasEntity;
 import br.com.daniel.spring_produtos.database.repository.IAlunosRespository;
 import br.com.daniel.spring_produtos.database.repository.IAvaliacoesFisicasRespository;
 import br.com.daniel.spring_produtos.dto.AvaliacaoFisicaDto;
+import br.com.daniel.spring_produtos.dto.AvaliacoesFisicasProjection;
 import br.com.daniel.spring_produtos.exception.BadRequestException;
 import br.com.daniel.spring_produtos.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliacaoFisicaService {
    private final IAlunosRespository alunosRespository;
-   private final IAvaliacoesFisicasRespository avaliacoesFisicasRespository;
+   private final IAvaliacoesFisicasRespository avaliacoesFisicasRepository;
 
    public void criarAvaliacaoFisica(AvaliacaoFisicaDto avaliacaoFisicaDto) throws NotFoundException, BadRequestException{
        AlunosEntity aluno = alunosRespository.findById(avaliacaoFisicaDto.getAlunoId())
@@ -34,4 +39,12 @@ public class AvaliacaoFisicaService {
        aluno.setAvaliacaoFisica(avaliacaoFisica);
        alunosRespository.save(aluno);
    }
+
+   public List<AvaliacoesFisicasProjection> getAllAvaliacoes(){
+       return avaliacoesFisicasRepository.getAllAvaliacoes();
+   }
+
+    public Page<AvaliacoesFisicasProjection> getAllAvaliacoesPageable(Integer page, Integer size){
+        return avaliacoesFisicasRepository.getAllAvaliacoesPageable(PageRequest.of(page, size));
+    }
 }
