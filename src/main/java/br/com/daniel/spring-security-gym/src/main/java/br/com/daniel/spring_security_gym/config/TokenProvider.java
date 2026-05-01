34f3d3b42c2a520c7e4b8ff6c4f662899a474a4e
dlzada <danielluiz1607@gmail.com>
2026-05-01 09:59:34 -0300

    feat: create methods in TokenProvider and modification name project in .yaml

--- src/main/java/br/com/daniel/spring-security-gym/src/main/java/br/com/daniel/spring_security_gym/config/TokenProvider.java
+++ src/main/java/br/com/daniel/spring-security-gym/src/main/java/br/com/daniel/spring_security_gym/config/TokenProvider.java
@@ -1,4 +1,66 @@
 package br.com.daniel.spring_security_gym.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class TokenProvider {
+    @Value("${jwt.expiration:600000}")
+    private long expirationTime;
+
+    @Value("${jwt.key}")
+    private String key;
+
+    //gerar um token
+    public String gerarToken(Authentication authentication){
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        return buildToken(user.getUsername());
+    }
+
+    private String buildToken(String username){
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(getSigninKey())
+                .compact();
+    }
+
+    private SecretKey getSigninKey(){
+        return Keys.hmacShaKeyFor(key.getBytes());
+    }
+
+    // validar um token
+    public boolean isTokenValid(String token){
+        try{
+            getClaims(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    //extrair informacoes do token
+    private String getUserName(String token){
+        return getClaims(token).getSubject();
+    }
 }
