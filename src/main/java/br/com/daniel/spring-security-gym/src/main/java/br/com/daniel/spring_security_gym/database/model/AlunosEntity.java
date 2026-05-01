6f21bb3a9dacbda3577ab8c6f0b862a1d52273bd
dlzada <danielluiz1607@gmail.com>
2026-05-01 09:16:22 -0300

    feat: implementation of interface UserDetails and your methos in AlunosEntity

--- src/main/java/br/com/daniel/spring-security-gym/src/main/java/br/com/daniel/spring_security_gym/database/model/AlunosEntity.java
+++ src/main/java/br/com/daniel/spring-security-gym/src/main/java/br/com/daniel/spring_security_gym/database/model/AlunosEntity.java
@@ -2,8 +2,12 @@ package br.com.daniel.spring_security_gym.database.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -13,7 +17,41 @@ import java.util.Set;
 @Getter
 @Setter
 @Builder
-public class AlunosEntity {
+public class AlunosEntity implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +62,7 @@ public class AlunosEntity {
 
     @Column(nullable = false, unique = true)
     private String email;
+    private String senha;
 
     @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
     @JoinColumn(name = "avaliacao_fisica_id")
@@ -31,6 +70,12 @@ public class AlunosEntity {
 
     @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
     private Set<TreinosEntity> treinos = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "alunos_roles",
+            joinColumns = @JoinColumn(name = "alunos_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RolesEntity> roles = new HashSet<>();
 
 }
 
